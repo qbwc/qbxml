8b27b31dacb1a56e66286ab837d6caa80dcae784
Jeff Moore <pair+jeff@mavenlink.com>
2016-07-15 13:40:08 -0700

    Handle parsing of % character in a float
    Apparently QuickBooks can sometimes return an actual `%` in a percentage field (e.g. "18.0%").  While this is in no way a reasonable float value whatsoever, I guess we should probably not throw an exception when confronted by the capricious whimsies of Intuit data formats.

--- test/unit/xml_to_hash_test.rb
+++ test/unit/xml_to_hash_test.rb
@@ -26,5 +26,80 @@ class XmlToHashTest < Minitest::Test
     assert_equal h, qbxml.from_qbxml("<?qbxml version=\"7.0\"?>\n<QBXML>\n  <QBXMLMsgsRq>\n    <InvoiceQueryRq>\n      <IncludeRetElement>TxnID</IncludeRetElement>\n    <IncludeRetElement>RefNumber</IncludeRetElement>\n    </InvoiceQueryRq>\n  </QBXMLMsgsRq>\n</QBXML>\n")
   end
 
-end
+  def test_float_percentage
+    qbxml = Qbxml.new
 
+    h = {
+      "qbxml" => {
+        "xml_attributes" => {},
+        "qbxml_msgs_rs" => {
+          "xml_attributes" => {},
+          "item_query_rs" => {
+            "xml_attributes" => {
+              "requestID" => "Retrieve items",
+              "statusCode" => "0",
+              "statusSeverity" => "Info",
+              "statusMessage" => "Status OK",
+              "iteratorRemainingCount" => "0",
+              "iteratorID" => "{10c05cbd-b25b-4a85-8aa0-8bce89e6e900}"
+            },
+            "item_service_ret" => {
+              "xml_attributes" => {},
+              "list_id" => "80000005-1468535148",
+              "time_created" => "2016-07-14T15:25:48-08:00",
+              "time_modified" => "2016-07-14T15:25:48-08:00",
+              "edit_sequence" => "1468535148",
+              "name" => "let's get intuit",
+              "full_name" => "let's get intuit",
+              "is_active" => true,
+              "sublevel" => 0,
+              "sales_or_purchase" => {
+                "xml_attributes" => {},
+                "price_percent" => 18.0,
+                "account_ref" => {
+                  "xml_attributes" => {},
+                  "list_id" => "80000015-1457547358",
+                  "full_name" => "Repairs and Maintenance"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    xml = <<-XML
+      <QBXML>
+        <QBXMLMsgsRs>
+          <ItemQueryRs requestID="Retrieve items"
+                       statusCode="0"
+                       statusSeverity="Info"
+                       statusMessage="Status OK"
+                       iteratorRemainingCount="0"
+                       iteratorID="{10c05cbd-b25b-4a85-8aa0-8bce89e6e900}">
+            <ItemServiceRet>
+              <ListID>80000005-1468535148</ListID>
+              <TimeCreated>2016-07-14T15:25:48-08:00</TimeCreated>
+              <TimeModified>2016-07-14T15:25:48-08:00</TimeModified>
+              <EditSequence>1468535148</EditSequence>
+              <Name>let's get intuit</Name>
+              <FullName>let's get intuit</FullName>
+              <IsActive>true</IsActive>
+              <Sublevel>0</Sublevel>
+              <SalesOrPurchase>
+                <PricePercent>18.0%</PricePercent>
+                <AccountRef>
+                  <ListID>80000015-1457547358</ListID>
+                  <FullName>Repairs and Maintenance</FullName>
+                </AccountRef>
+              </SalesOrPurchase>
+            </ItemServiceRet>
+          </ItemQueryRs>
+        </QBXMLMsgsRs>
+      </QBXML>
+    XML
+
+    assert_equal h, qbxml.from_qbxml(xml)
+  end
+
+end
