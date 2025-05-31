9b29f5640d6f77f677c62755fc8c8ee2b508b18b
Jason Barnabe <jason.barnabe@gmail.com>
2025-05-31 15:57:34 -0500

    All of activesupport maybe?

--- lib/qbxml.rb
+++ lib/qbxml.rb
@@ -1,8 +1,8 @@
 require "qbxml/version"
 
 require 'nokogiri'
+require 'active_support'
 require 'active_support/builder'
-require 'active_support/deprecator'
 require 'active_support/core_ext/string'
 require 'active_support/core_ext/object/blank.rb'
 
